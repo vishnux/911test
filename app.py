2a813b48f3b30b5595a9f7cc719fe0ba64c60002
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-02-07 21:27:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.bar_chart(filtered_data['Response Lag Time'])
 
 # Show statistics on response lag times
 col1, col2, col3 = st.columns(3)
-col1.metric("Mean Response Lag Time:", filtered_data['Response Lag Time'].mean())
+col1.metric("Mean Response Lag Time:", round(filtered_data['Response Lag Time'].mean(),2))
 col2.metric("Median Response Lag Time:", filtered_data['Response Lag Time'].median())
 col3.metric("Standard Deviation of Response Lag Time:", filtered_data['Response Lag Time'].std())
 
