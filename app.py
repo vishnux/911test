2bc63c1af8bad2b94eb90c85dc5749a14766221d
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-02-18 19:24:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -25,15 +25,13 @@ st.write("Response Lag Time (in minutes)")
 st.bar_chart(filtered_data['Response Lag Time'])
 
 # Show statistics on response lag times
-col1, col2, col3 = st.columns((1,1,1))#st.columns(3)
+col0, col1, col2, col3, col4 = st.columns((1,1,1,1,1))
+col0.write('')
 col1.metric("Mean Response Lag Time:", round(filtered_data['Response Lag Time'].mean(),2))
 col2.metric("Median Response Lag Time:", round(filtered_data['Response Lag Time'].median(),2))
 col3.metric("SD of Response Lag Time:", round(filtered_data['Response Lag Time'].std(),2))
+col4.write('')
 
-col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "70 °F", "1.2 °F")
-col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
 
 # Show a table of top 5 Fire Stations with highest mean response lag times
 st.write("Top 5 Fire Stations with highest mean response lag times:")
