823528632b925e6c0bac460e0eec9e98445ed342
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-02-18 20:18:47 -0700

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ st.title("Calgary Fire Station Response Lag Time Analysis")
 
 # Load data into a pandas dataframe
 data = pd.read_csv("fire_station_data.csv")
-df_ems = pd.read_excel("Fire_Stations.xlsx")
+df_fire = pd.read_excel("Fire_Stations.csv")
 #Map
-st.map(df_ems)
+st.map(df_fire)
 
 # Filter data for FSA level
 selected_level = st.selectbox("Select FSA Level", options=data['FSA'].unique())
