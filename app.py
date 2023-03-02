5f19eb28138dbeda841fb52ad3eacd98a2ed8561
Vishnu R Nair <60481100+vishnux@users.noreply.github.com>
2023-03-01 21:48:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ shapefile = gpd.read_file("clipped-to-calgary.shp")
 #m = folium.Map(tiles='OpenStreetMap',zoom_start=160)
 m = shapefile.explore()
 for idx, row in df_fire.iterrows():
-    folium.Marker(location=[row["Latitude"], row["Longitude"]], popup=row["Name"]).add_to(m)
+    folium.Marker(location=[row["Lat"], row["Lon"]], popup=row["Name"]).add_to(m)
 st_data = st_folium(m, width=725)
 shapefile.explore()
 
